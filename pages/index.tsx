df687a40b2d63ff3d953b9710fbd8d3586468fcd
Yoonsung Baek <91772497+bysxx@users.noreply.github.com>
2022-10-17 21:36:38 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+import styled from 'styled-components';
+import Post from '@components/Compound/Post'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Post>
+        <Post.Title />
+        <Post.Comment />
+        <Post.Buttons />
+      </Post>
+    </Container>
+  );
+};
+
+export default Home;
